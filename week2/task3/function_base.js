2573f8b3d28cf01e3743334c4614b82c1ce07cdc
Thapa Chhetri Bal krishna <s2300090@BC-C02J64ASQ6LR-MacBook-Air.local>
2023-01-24 14:01:56 +0200

    Margit Js lessons

---
+++ week2/task3/function_base.js
@@ -0,0 +1,15 @@
+function myFunction1(c, a, b) {
+    return a + b + c; 
+}
+function myFunction2() {
+    return 'Hello World';  
+}
+function myFunction2(a) {
+    return 'Hello World';
+}
+function myFunction3() {
+    return 'Hello World';
+}
+myFunction1(1, 2, 3);
+myFunction2( );
+myFunction3(a);
