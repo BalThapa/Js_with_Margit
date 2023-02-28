b87d870cd9964ceb4b5af6e960a8ee1b9727993a
BalThapa <info.balthapa@gmail.com>
2023-02-28 17:29:10 +0200

    catch_the_frog

--- week5/Speed_game/main.js
+++ week5/Speed_game/main.js
@@ -5,6 +5,8 @@ const closeButton = document.querySelector('#close')
 const scoreSpan = document.querySelector('.score')
 const scoreEnd = document.querySelector('.scoreEnd')
 const overlay = document.querySelector('.overlay')
+const startSound = new Audio('frog_quak-81741 copy.mp3')
+const endSound = new Audio('game finish.wav')
 
 
 let score = 0
@@ -12,7 +14,6 @@ let active = 0
 let timer
 let pace = 1000
 let rounds = 0
-
 
 circles.forEach((circle, i) => {
   circle.addEventListener('click', () => clickCircle(i))
@@ -36,10 +37,12 @@ const enableCircles = () => {
 }
 
 const startGame = () => {
+  const startSound = new Audio('frog_quak-81741 copy.mp3');
+  startSound.play()
   if (rounds >= 10) {
-   return endGame()
+  return endGame()
   }
-
+  
   startButton.classList.add('hidden')
   endButton.classList.remove('hidden')
 
@@ -67,6 +70,10 @@ const startGame = () => {
 }
 
 const endGame = () => {
+  const startSound = new Audio('frog_quak-81741 copy.mp3');
+  startSound.pause()
+  const endSound = new Audio('game finish.wav');
+  endSound.play()
   scoreEnd.textContent = score
   endButton.classList.remove('hidden')
   startButton.classList.add('hidden')
@@ -78,9 +85,6 @@ const resetGame = () => {
   window.location.reload()
 }
 
-
-
 startButton.addEventListener('click', startGame)
 endButton.addEventListener('click', endGame)
 closeButton.addEventListener('click', resetGame)
-
