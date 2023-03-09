5a1d9698dccbd675478175497e8d9244d8e99002
BalThapa <info.balthapa@gmail.com>
2023-03-09 21:56:01 +0200

    carPairTask

--- week6/main.js
+++ week6/main.js
@@ -1,13 +1,24 @@
-// fetch('https://jsonplaceholder.typicode.com/todos/')
-//   .then(response => response.json())
-//   .then(json => console.log(json))
+const form = document.querySelector('form').value
+const licence = document.querySelector('#licence')
+const model = document.querySelector('#model')
+const make = document.querySelector('#make')
+const owner = document.querySelector('#owner')
+const price = document.querySelector('#price')
+const color = document.querySelector('#color')
+const addCar = document.querySelector('.addCar')
+const submitCar = document.querySelector('#btn_submit')
+const table = document.querySelector('table')
 
 
-fetch('https://jsonplaceholder.typicode.com/users/')
-  .then(response => response.json())
-  .then(json => { dataList(json); console.log(json); })
-const dataList = (data) => {
-  document.querySelector('.data').innerHTML = data.map(item => {
-    return `<li>${item.name} - ${item.phone} <a href="mailto:${item.email}"> ${item.email} </a></li > `
-  }).join('');
-}
+const addedCar = (e) => {
+    e.preventDefault();
+    localStorage.setItem('Licence Number',licence.value)
+    localStorage.setItem('Car Make',make.value)
+    localStorage.setItem('Car Model',model.value)
+    localStorage.setItem('Car Owner',owner.value)
+    localStorage.setItem('Car Price',price.value)
+    localStorage.setItem('Car Color',color.value)
+};
+
+addCar.addEventListener('click',addedCar);
+
